6164b45d169e374d813926b8385f01f6c157bd8d
suriyanarayanan <ssram068@gmail.com>
2021-05-31 15:06:38 +0530

    staff login feature added

---
+++ src/main/java/in/suriya/sql.sql
@@ -0,0 +1,27 @@
+create table staff(
+name varchar(50) not null,
+mobno bigint not null unique,
+password varchar(50) not null unique
+);
+
+insert into staff (name,mobno,password) values('Harish',9003750359,'Harish1#');
+insert into staff (name,mobno,password) values('Suriya',9500500452,'Suriya1@');
+select *from staff;
+delete from staff;
+drop table staff;
+
+
+create table student(
+rollno bigint unique,
+mobno bigint not null,
+fee int not null,
+payment_status varchar(20) default 'Not_paid'
+);
+
+insert into student(rollno,mobno,fee) values('1712114',9500500451,75000);
+insert into student(rollno,mobno,fee) values('1712113',9003750351,60000);
+
+
+select *from student;
+delete from student;
+drop table student;
